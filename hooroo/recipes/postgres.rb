88536ffa42cf5c59156321c142b5c6b14d150368
Ash McKenzie <ash@greenworm.com.au>
2013-11-12 17:10:17 +1100

    Postgres role and database setup

--- hooroo/recipes/postgres.rb
+++ hooroo/recipes/postgres.rb
@@ -1,11 +1,43 @@
-#
 # Cookbook Name:: hooroo
 # Recipe:: postgres
 #
 # Copyright 2013, Hooroo
-cookbook_file "hba" do
-	source "pg_hba.conf"
-	owner "postgres"
-	group "postgres"
-	mode 00644
+
+file '/etc/postgresql/9.2/main/pg_hba.conf' do
+  owner 'postgres'
+  group 'postgres'
+  mode '600'
+  source 'pg_hba.conf'
+end
+
+node[:deploy].each do |application|
+
+  database_details = node[:deploy][application].fetch(:database, false)
+
+  if database_details
+
+    db_database = database_details[:database]
+    db_username = database_details[:username]
+    db_password = database_details[:password]
+
+    bash "Setup PostgreSQL roles" do
+      user "postgres"
+      cwd "/tmp"
+      code <<-EOH
+        psql postgres -c "CREATE ROLE #{db_username} WITH PASSWORD '#{db_password}' LOGIN"
+      EOH
+
+      not_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_roles WHERE rolname='#{db_username}'"`x == "1x" }
+    end
+
+    bash "Create PostgreSQL databases" do
+      user "postgres"
+      cwd "/tmp"
+      code <<-EOH
+        psql postgres -c "CREATE DATABASE #{db_database} OWNER #{db_username}"
+      EOH
+
+      not_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_database WHERE datname='#{db_database}'"`x == "1x" }
+    end
+  end
 end
