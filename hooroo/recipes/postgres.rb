326fb8dd71904c71b0b8fecba2d7fe2ddc71110b
Ash McKenzie <ash@greenworm.com.au>
2013-11-12 18:47:53 +1100

    ugh, long day

--- hooroo/recipes/postgres.rb
+++ hooroo/recipes/postgres.rb
@@ -9,13 +9,13 @@
 #
 include_recipe 'postgresql::default'
 
-default['postgresql']['pg_hba'] = [
-  { type: 'local', db: 'all', user: 'postgres', addr: nil, method: 'ident' },
-  { type: 'local', db: 'all', user: 'all', addr: nil, method: 'ident' },
-  { type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'md5' },
-  { type: 'host', db: 'all', user: 'all', addr: '10.0.0.1/16', method: 'md5' },
-  { type: 'host', db: 'all', user: 'all', addr: '::1/128', method: 'md5' },
-  { type: 'host', db: 'all', user: 'all', addr: '10.0.0.0/8', method: 'md5' }
+override['postgresql']['pg_hba'] = [
+  { :type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident' },
+  { :type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.1/16', :method => 'md5' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5' }
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.0/8', :method => 'md5' }
 ]
 
 # cookbook_file "#{node['postgresql']['dir']}/pg_hba.conf" do
