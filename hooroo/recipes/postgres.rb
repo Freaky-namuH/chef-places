b7aa9ab5a1f73c298d1a1e8c1f5d506d352fdd56
Ash McKenzie <ash@greenworm.com.au>
2013-11-13 15:09:01 +1100

    I've got it!

--- hooroo/recipes/postgres.rb
+++ hooroo/recipes/postgres.rb
@@ -7,26 +7,18 @@
 # app servers to talk to the db servers even though the postgres recipe
 # manages the pg_hba.conf but doesn't allow it to be enhanced :(
 #
-# include_recipe 'apt::default'
-# include_recipe 'postgresql::default'
+include_recipe 'postgresql::server'
 
-# override['postgresql']['pg_hba'] = [
-#   { :type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident' },
-#   { :type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident' },
-#   { :type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5' },
-#   { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.1/16', :method => 'md5' },
-#   { :type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5' },
-#   { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.0/8', :method => 'md5' }
-# ]
+node.override['postgresql']['pg_hba'] = [
+  { :type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident' },
+  { :type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.1/16', :method => 'md5' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5' },
+  { :type => 'host', :db => 'all', :user => 'all', :addr => '10.0.0.0/8', :method => 'md5' }
+]
 
-# cookbook_file "#{node['postgresql']['dir']}/pg_hba.conf" do
-#   owner 'postgres'
-#   group 'postgres'
-#   mode '600'
-#   source 'pg_hba.conf'
-# end
-
-node[:deploy].each do |application|
+node[:deploy].each do |application, x|
 
   database_details = node[:deploy][application].fetch(:database, false)
 
@@ -43,7 +35,7 @@ node[:deploy].each do |application|
         psql postgres -c "CREATE ROLE #{db_username} WITH PASSWORD '#{db_password}' LOGIN"
       EOH
 
-      not_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_roles WHERE rolname='#{db_username}'"`x == "1x" }
+      only_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_roles WHERE rolname='#{db_username}'"`x != "1x" }, :user => 'postgres'
     end
 
     bash "Create PostgreSQL databases" do
@@ -53,7 +45,7 @@ node[:deploy].each do |application|
         psql postgres -c "CREATE DATABASE #{db_database} OWNER #{db_username}"
       EOH
 
-      not_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_database WHERE datname='#{db_database}'"`x == "1x" }
+      only_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_database WHERE datname='#{db_database}'"`x != "1x" }, :user => 'postgres'
     end
   end
 end
