7e768c3877dbc6ce57f64be6eb84a9aedc6b74c2
Ash McKenzie <ash@greenworm.com.au>
2013-11-20 08:21:09 +1100

    Not great, but moved performance options into nginx.conf.erb

--- hooroo/recipes/nginx.rb
+++ hooroo/recipes/nginx.rb
@@ -8,15 +8,3 @@ include_recipe 'nginx'
 node.override['nginx']['event'] = 'epoll'
 node.override['nginx']['worker_connections'] = 10240
 node.override['nginx']['keepalive_timeout'] = 30
-
-# need to fit in somehow
-#
-# timer_resolution = '500ms'
-# worker_rlimit_nofile = 10240
-
-# server_name_in_redirect = 'off'
-# server_tokens = 'off'
-
-# client_body_timeout = 10
-# client_header_timeout = 10
-# client_header_buffer_size = 128
