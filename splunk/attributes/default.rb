831cc8eeb8b08f9c7f48e92d31438271915e39f3
Ash McKenzie <ash@greenworm.com.au>
2013-11-21 09:01:28 +1100

    First cut at getting splunk up and running

---
+++ splunk/attributes/default.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+splunk :version => "6.0",
+  :remote_file => "splunkforwarder-6.0-182037-Linux-i686.tgz",
+  :remote_url => "http://www.splunk.com/page/download_track?file=6.0/universalforwarder/linux/splunkforwarder-6.0-182037-Linux-i686.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86&version=6.0&product=splunkd&typed=release&elq=463e95fc-b7b5-453d-bb81-e589d16ea93b"
