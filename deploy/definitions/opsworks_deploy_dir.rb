e1151fd60310c3402aead873091ff9957ef1a9a6
Ash McKenzie <ash@greenworm.com.au>
2013-11-25 18:40:01 +1100

    Lets try and overwrite :opsworks_deploy_dir

---
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -0,0 +1,30 @@
+define :opsworks_deploy_dir do
+
+  directory "#{params[:path]}" do
+    group params[:group]
+    owner params[:user]
+    mode 0770
+    action :create
+    recursive true
+  end
+
+  directory "#{params[:path]}/shared" do
+    group params[:group]
+    owner params[:user]
+    mode 0770
+    action :create
+    recursive true
+  end
+
+  # create shared/ directory structure
+  ['log','config','system','pids','scripts','sockets'].each do |dir_name|
+    directory "#{params[:path]}/shared/#{dir_name}" do
+      group params[:group]
+      owner params[:user]
+      mode 0770
+      action :create
+      recursive true
+    end
+  end
+
+end
